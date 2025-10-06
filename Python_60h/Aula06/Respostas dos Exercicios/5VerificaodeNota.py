1747624379f0f6b93fdc28c513794614e9a9a7e0
luisfernandospoljaric <luisfernandospoljaric@gmail.com>
2025-10-06 08:50:54 -0300

    att

--- Python_60h/Aula06/Respostas dos Exercicios/5VerificaodeNota.py
+++
@@ -1,9 +0,0 @@
-nota = float(input("Digite sua nota (0-100): "))
-
-if 0 <= nota <= 100:
-    if nota >= 60:
-        print("Você foi aprovado")
-    else:
-        print("Você foi reprovado")
-else:
-    print("Nota inválida")
