68b7659b7fdc0c9a011f7e405995152a0771c810
luisfernandospoljaric <luisfernandospoljaric@gmail.com>
2025-12-01 09:38:20 -0300

    att

--- Python_60h/Aula14/Soluções dos Exercicios/Ex1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("produtos.xlsx")
-
-print(df.head())
--- Python_60h/Aula14/Soluções dos Exercicios/ex4.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df_estoque = pd.read_excel("estoque.xlsx")
-
-df_estoque['ValorTotal'] = df_estoque['Preco'] * df_estoque['Quantidade']
-
-df_estoque.to_excel("estoque_atualizado.xlsx", index=False)
